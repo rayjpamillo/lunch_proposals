1be191b989c693b790bb9bbf69810d53f0e6042e
unknown <maquiran@5CG5090MNH.nsn-intra.net>
2016-03-30 18:52:37 +0800

    Proposal Form Icon Proposal List Temp

--- public/src/home/home.component.ts
+++ public/src/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from 'angular2/core';
 import { Router } from 'angular2/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ProposalFormComponent } from '../proposal-form/proposal-form.component';
+import { ProposalListComponent } from '../proposal-list/proposal-list.component';
 
 @Component({
     selector: 'home',
     templateUrl: 'public/src/home/home.component.html',
-    directives: [NavbarComponent, ProposalFormComponent]
+    directives: [NavbarComponent, ProposalFormComponent, ProposalListComponent]
 })
 
 export class HomeComponent{
---
+++ public/src/proposal-list/proposal-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from 'angular2/core';
+
+@Component({
+selector: 'proposal-list',
+templateUrl: 'public/src/proposal-list/proposal-list.component.html'
+})
+
+export class ProposalListComponent { }
