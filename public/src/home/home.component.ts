4bb6d457bac38e63c5ff8497662df94ff27785d3
brigoli <giovanni.brigoli@nokia.com>
2016-03-30 15:13:59 +0800

    added login component

---
+++ public/src/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from 'angular2/core';
+
+@Component({
+    selector: 'home',
+    templateUrl: 'public/src/home/home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
---
+++ public/src/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from 'angular2/core';
+import {RouteParams, Router} from 'angular2/router';
+
+@Component({
+    selector: 'login-form',
+    templateUrl: 'public/src/login/login.component.html'
+})
+
+export class LoginComponent {
+    submitted = false;
+    notice = "You are about to access a private system. This system is for the use of authorized users only. All connections are logged. Any unauthorized access or attempts may be punished to the fullest extent possible under the applicable local legislation.";
+    
+    constructor(private router: Router){
+        
+    }
+    
+    onSubmit(){
+        this.submitted = true;
+        this.router.navigate(['Home']);
+    }
+}
